d76ddb0944a660105fdd47136d8c321f875beb86
João Gouveia <j.lucas.gouveia@gmail.com>
2022-01-09 10:27:09 +0000

    font awesome icons fixing

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@fortawesome/fontawesome-free/css/all"
 import '../js/bootstrap_js_files.js'
 
 const images = require.context('../images', true)
